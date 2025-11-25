aeec21622eb67cd8d4f8b796907539ff18bdc057
Louis Stalet <staletlouis@gmail.com>
2025-11-25 18:28:50 -0500

    created a minimax agent, will improve it into a alpha beta algorithm

--- agents/__init__.py
+++ agents/__init__.py
@@ -2,3 +2,5 @@ from .random_agent import RandomAgent
 from .human_agent import HumanAgent
 from .student_agent import StudentAgent
 from .greedy_corners_agent import StudentAgent
+from .alpha_beta_agent import StudentAgent
+from .minimax_agent import StudentAgent
---
+++ agents/minimax_agent.py
@@ -0,0 +1,99 @@
+# Student agent: Add your own agent here
+from agents.agent import Agent
+from store import register_agent
+import sys
+import numpy as np
+from copy import deepcopy
+import time
+from helpers import random_move, execute_move, check_endgame, get_valid_moves
+
+@register_agent("minimax_agent")
+class StudentAgent(Agent):
+  """
+  A class for your implementation. Feel free to use this class to
+  add any helper functionalities needed for your agent.
+  """
+      
+  def minimax(self, chess_board, player, opponent, depth, max_player):
+    done, p1, p2 = check_endgame(chess_board)
+    if depth == 0 or done:
+      return p1 - p2 if max_player == 1 else p2 - p1
+    all_moves = get_valid_moves(chess_board, player)
+    if len(all_moves) == 0:
+      return self.minimax(chess_board, opponent, player, depth - 1, max_player)
+    
+    if player == max_player:
+      best_move = float('-inf')
+      for i in all_moves:
+        temp_board = deepcopy(chess_board)
+        execute_move(temp_board, i, player)
+        move_value = self.minimax(temp_board, opponent, player, depth - 1, max_player)
+        if move_value > best_move:
+          best_move = move_value
+    else:
+        best_move = float('inf')
+        for i in all_moves:
+          temp_board = deepcopy(chess_board)
+          execute_move(temp_board, i, player)
+          move_value = self.minimax(temp_board, opponent, player, depth - 1, max_player)
+          if move_value < best_move:
+            best_move = move_value
+
+    return best_move
+
+  def find_best_move(self, chess_board, player, opponent, depth):
+    all_moves = get_valid_moves(chess_board, player)
+    best_move = all_moves[0]
+    best_score = float('-inf')
+    max_player = player
+    if len(all_moves) == 0: 
+      return None
+    for i in all_moves:
+      temp_board = deepcopy(chess_board)
+      execute_move(temp_board, i, player)
+
+      move_value = self.minimax(temp_board, opponent, player, depth - 1, max_player)
+      if move_value > best_score:
+        best_score = move_value
+        best_move = i
+    return best_move 
+    
+
+    
+
+
+  def __init__(self):
+    super(StudentAgent, self).__init__()
+    self.name = "MinimaxAgent"
+    self.max_depth = 2
+
+  def step(self, chess_board, player, opponent):
+    """
+    Implement the step function of your agent here.
+    You can use the following variables to access the chess board:
+    - chess_board: a numpy array of shape (board_size, board_size)
+      where 0 represents an empty spot, 1 represents Player 1's discs (Blue),
+      and 2 represents Player 2's discs (Brown).
+    - player: 1 if this agent is playing as Player 1 (Blue), or 2 if playing as Player 2 (Brown).
+    - opponent: 1 if the opponent is Player 1 (Blue), or 2 if the opponent is Player 2 (Brown).
+
+    You should return a tuple (r,c), where (r,c) is the position where your agent
+    wants to place the next disc. Use functions in helpers to determine valid moves
+    and more helpful tools.
+
+    Please check the sample implementation in agents/random_agent.py or agents/human_agent.py for more details.
+    """
+
+    # Some simple code to help you with timing. Consider checking 
+    # time_taken during your search and breaking with the best answer
+    # so far when it nears 2 seconds.
+    start_time = time.time()
+    move = self.find_best_move(chess_board, player, opponent, self.max_depth)
+    time_taken = time.time() - start_time
+
+    print("My AI's turn took ", time_taken, "seconds.")
+
+    # Dummy return (you should replace this with your actual logic)
+    # Returning a random valid move as an example
+    return move
+
